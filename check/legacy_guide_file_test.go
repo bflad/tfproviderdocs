2075c8f2f281e3f48b71a747f4280a64b4d737a5
Brian Flad <bflad417@gmail.com>
2019-12-16 15:16:13 -0500

    check: Add unit testing for LegacyGuideFileCheck
    Closes #1

---
+++ check/legacy_guide_file_test.go
@@ -0,0 +1,63 @@
+package check
+
+import (
+	"testing"
+)
+
+func TestLegacyGuideFileCheck(t *testing.T) {
+	testCases := []struct {
+		Name        string
+		BasePath    string
+		Path        string
+		Options     *LegacyGuideFileOptions
+		ExpectError bool
+	}{
+		{
+			Name:     "valid",
+			BasePath: "testdata/valid-legacy-files",
+			Path:     "guide.html.markdown",
+		},
+		{
+			Name:        "invalid extension",
+			BasePath:    "testdata/invalid-legacy-files",
+			Path:        "guide_invalid_extension.txt",
+			ExpectError: true,
+		},
+		{
+			Name:        "invalid frontmatter",
+			BasePath:    "testdata/invalid-legacy-files",
+			Path:        "guide_invalid_frontmatter.html.markdown",
+			ExpectError: true,
+		},
+		{
+			Name:        "invalid frontmatter without layout",
+			BasePath:    "testdata/invalid-legacy-files",
+			Path:        "guide_without_layout.html.markdown",
+			ExpectError: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			if testCase.Options == nil {
+				testCase.Options = &LegacyGuideFileOptions{}
+			}
+
+			if testCase.Options.FileOptions == nil {
+				testCase.Options.FileOptions = &FileOptions{
+					BasePath: testCase.BasePath,
+				}
+			}
+
+			got := NewLegacyGuideFileCheck(testCase.Options).Run(testCase.Path)
+
+			if got == nil && testCase.ExpectError {
+				t.Errorf("expected error, got no error")
+			}
+
+			if got != nil && !testCase.ExpectError {
+				t.Errorf("expected no error, got error: %s", got)
+			}
+		})
+	}
+}
