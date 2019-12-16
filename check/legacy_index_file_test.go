2535e5d4eee1f85731c0892a3b829b1fc6d4566a
Brian Flad <bflad417@gmail.com>
2019-12-16 15:21:24 -0500

    check: Add unit testing for LegacyIndexFileCheck
    Closes #2

---
+++ check/legacy_index_file_test.go
@@ -0,0 +1,63 @@
+package check
+
+import (
+	"testing"
+)
+
+func TestLegacyIndexFileCheck(t *testing.T) {
+	testCases := []struct {
+		Name        string
+		BasePath    string
+		Path        string
+		Options     *LegacyIndexFileOptions
+		ExpectError bool
+	}{
+		{
+			Name:     "valid",
+			BasePath: "testdata/valid-legacy-files",
+			Path:     "index.html.markdown",
+		},
+		{
+			Name:        "invalid extension",
+			BasePath:    "testdata/invalid-legacy-files",
+			Path:        "index_invalid_extension.txt",
+			ExpectError: true,
+		},
+		{
+			Name:        "invalid frontmatter",
+			BasePath:    "testdata/invalid-legacy-files",
+			Path:        "index_invalid_frontmatter.html.markdown",
+			ExpectError: true,
+		},
+		{
+			Name:        "invalid frontmatter without layout",
+			BasePath:    "testdata/invalid-legacy-files",
+			Path:        "index_without_layout.html.markdown",
+			ExpectError: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			if testCase.Options == nil {
+				testCase.Options = &LegacyIndexFileOptions{}
+			}
+
+			if testCase.Options.FileOptions == nil {
+				testCase.Options.FileOptions = &FileOptions{
+					BasePath: testCase.BasePath,
+				}
+			}
+
+			got := NewLegacyIndexFileCheck(testCase.Options).Run(testCase.Path)
+
+			if got == nil && testCase.ExpectError {
+				t.Errorf("expected error, got no error")
+			}
+
+			if got != nil && !testCase.ExpectError {
+				t.Errorf("expected no error, got error: %s", got)
+			}
+		})
+	}
+}
