ad20aa25fc869c17cabf26a45561ac3d320bddd8
kt <kt@katbyte.me>
2020-01-08 08:23:13 -0800

    address pr comments

--- check/file_extension.go
+++ check/file_extension.go
@@ -26,7 +26,7 @@ var ValidRegistryFileExtensions = []string{
 
 func LegacyFileExtensionCheck(path string) error {
 	if !FilePathEndsWithExtensionFrom(path, ValidLegacyFileExtensions) {
-		return fmt.Errorf("file does not end with a valid extension (%s), valid values: %v", GetFileExtension(path), ValidLegacyFileExtensions)
+		return fmt.Errorf("file does not end with a valid extension, valid extensions: %v", ValidLegacyFileExtensions)
 	}
 
 	return nil
@@ -34,28 +34,10 @@ func LegacyFileExtensionCheck(path string) error {
 
 func RegistryFileExtensionCheck(path string) error {
 	if !FilePathEndsWithExtensionFrom(path, ValidRegistryFileExtensions) {
-		return fmt.Errorf("file does not end with a valid extension (%s), valid values: %v", GetFileExtension(path), ValidLegacyFileExtensions)
+		return fmt.Errorf("file does not end with a valid extension, valid extensions: %v", ValidLegacyFileExtensions)
 	}
 
 	return nil
-}
-
-// GetFileExtension fetches file extensions including those with multiple periods.
-// This is a replacement for filepath.Ext(), which only returns the final period and extension.
-func GetFileExtension(path string) string {
-	filename := filepath.Base(path)
-
-	if filename == "." {
-		return ""
-	}
-
-	dotIndex := strings.IndexByte(filename, '.')
-
-	if dotIndex > 0 {
-		return filename[dotIndex:]
-	}
-
-	return filename
 }
 
 func FilePathEndsWithExtensionFrom(path string, validExtensions []string) bool {
