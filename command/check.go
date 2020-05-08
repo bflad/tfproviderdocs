1fecab62daa0807566a1656034fc41ea855e8311
Brian Flad <bflad417@gmail.com>
2020-05-08 01:07:41 -0400

    check: Output actual bytes

--- command/check.go
+++ command/check.go
@@ -320,7 +320,7 @@ func providerSchemas(path string) (*tfjson.ProviderSchemas, error) {
 
 	if err := json.Unmarshal(content, &ps); err != nil {
 		if syntaxError, ok := err.(*json.SyntaxError); ok {
-			return nil, fmt.Errorf("error parsing providers schema JSON file (%s): syntax error at byte offset (%d):\noriginal bytes: %v\noriginal error: %w", path, syntaxError.Offset, string(content[0:syntaxError.Offset]), err)
+			return nil, fmt.Errorf("error parsing providers schema JSON file (%s): syntax error at byte offset (%d):\noriginal string: %v\noriginal bytes: %#v\noriginal error: %w", path, syntaxError.Offset, string(content[0:syntaxError.Offset]), content[0:syntaxError.Offset], err)
 		}
 
 		return nil, fmt.Errorf("error parsing providers schema JSON file (%s): %w", path, err)
